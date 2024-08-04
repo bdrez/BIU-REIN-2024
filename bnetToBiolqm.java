fdc273dbf8ad0f4941f12caccb25a52e7aaa2131
bdrez <159343619+bdrez@users.noreply.github.com>
2024-08-04 08:14:19 -0400

    Create bnetToBiolqm.java

---
+++ bnetToBiolqm.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class bnetToBiolqm  {
+    //make a code the automates the bnet file to be put into the biolqm
+    //check for v_ and make sure their all defined , if not add at the end and define it
+    //if there's a comma with no space after, add a space
+    // add all genes to a hashset. If the gene isn't in the target column, add gene, gene
+
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        File file = new File("C:\\Users\\blimy\\OneDrive\\Desktop\\demo11.bnet");
+        String newBnetFilePath = "C:\\Users\\blimy\\Downloads\\outputFile2.bnet";
+        BufferedWriter bnetWriter = new BufferedWriter(new FileWriter(newBnetFilePath));
+        Scanner sc = new Scanner(file);
+
+        //take in the first sentence and make a space between
+        String line=sc.nextLine();
+        line=line.replace(",",", ");
+        bnetWriter.write(line);
+        bnetWriter.newLine();
+
+        System.out.println(line);
+
+        HashSet<String> geneTargets = new HashSet<>();
+        HashSet<String> geneFactors= new HashSet<>();
+        //HashSet<String> extraFactors = new HashSet<>();
+        while (sc.hasNextLine()){
+            String lines=sc.nextLine();
+            bnetWriter.write(lines);
+            bnetWriter.newLine();
+            String splitLines[]=lines.split(",");
+            geneTargets.add(splitLines[0].trim());
+            //now we go through the second part of the split and if something start with v_ add it
+            Pattern genePattern = Pattern.compile("v_[a-zA-Z0-9_]+");
+            Matcher geneMatch = genePattern.matcher(splitLines[1]);
+
+            while (geneMatch.find()){
+                geneFactors.add(geneMatch.group());
+            }
+
+        }
+
+        for (String gene : geneFactors) {
+            if (!geneTargets.contains(gene)) {
+                bnetWriter.write(gene + ", " + gene);
+                bnetWriter.newLine();
+                //extraFactors.add(gene);
+            }
+        }
+
+        System.out.println("targets" +geneTargets);
+        System.out.println("factor" +geneFactors);
+
+        bnetWriter.close();
+        //System.out.println("missed targ"+ extraFactors);
+
+        // add all the defined targets to a set, and then go through each stnence and look at the factor if
+        //their not in the set then we can add either to the next line or at the end equal to itself?
+
+    }
+
+
+}
